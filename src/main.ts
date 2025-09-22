fbd8fa3064cc615665b65ea9882e193ab99560b9
Towanou Luc <towanouluc@gmail.com>
2025-09-22 11:23:32 +0100

    suprimer le port de render.yaml

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
-    console.log(`🚀 Server running on port ${process.env.PORT || 3000}`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  // await app.listen(port ?? 3000, '0.0.0.0');
+  console.log(`🚀 Server running on port ${port || 3000}`);
 }
 bootstrap();
