e5908e6c8cfddb2ec3ed780ec338cc07633b1eca
Towanou Luc <towanouluc@gmail.com>
2025-10-01 15:33:57 +0100

    mise à jours des dependances_2

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { BigIntInterceptor } from './common/interceptors/bigint.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Ajout de l’interceptor global
+  app.useGlobalInterceptors(new BigIntInterceptor()); 
+  
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`Listening on ${port}`);
 
-   // Ajout de l’interceptor global
-  app.useGlobalInterceptors(new BigIntInterceptor()); 
+   
 }
 bootstrap();
 // async function bootstrap() {
